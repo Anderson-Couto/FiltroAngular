89cbcf150553afd1506afc906836d04aeb634096
Anderson Couto <andersoncouto57@gmail.com>
2021-02-10 22:38:30 -0300

    Input Hint

---
+++ src/app/Models/Dicionario.ts
@@ -0,0 +1,13 @@
+export class Dicionario
+{
+
+    vogais = new Map([
+        [0, 'A'],
+        [1, 'E'],
+        [2, 'I'],
+        [3, 'O'],
+        [4, 'U'],
+        [34, 'OU'],
+    ])
+    
+}
--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Dicionario } from 'src/app/Models/Dicionario';
 
 @Component({
   selector: 'app-home',
@@ -14,5 +15,25 @@ export class HomeComponent implements OnInit {
 
   value = 0;
   teste=0;
+
+  traduz(num: HTMLInputElement)
+  { 
+    //let vogaisDict = new Map();
+    //vogaisDict.set(0, 'A')
+    //vogaisDict.set(1, 'E')
+    
+    let vogaisDict = new Dicionario
+    let valor = Number(num.value)
+
+    var condicao = vogaisDict.vogais.has(valor)
+
+    if (num.value && condicao) {
+      return vogaisDict.vogais.get(valor)
+    }
+
+    console.log(condicao)
+
+    return;
+  }
 
 }
